149761f54a799ae9e854d3760bdb7054cd3af456
aayush <aayush.1si14cs001@gmail.com>
2020-06-09 02:56:42 +0530

    Setting Entities and Ids in JPA

--- src/main/java/com/example/springwebapp/domain/Author.java
+++ src/main/java/com/example/springwebapp/domain/Author.java
@@ -1,10 +1,21 @@
 package com.example.springwebapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
+import java.util.Objects;
 import java.util.Set;
 
+@Entity
 public class Author {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstname;
     private String lastname;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author(String firstname, String lastname, Set<Book> books) {
@@ -14,6 +25,14 @@ public class Author {
     }
 
     public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstname() {
@@ -38,5 +57,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/com/example/springwebapp/domain/Book.java
+++ src/main/java/com/example/springwebapp/domain/Book.java
@@ -1,11 +1,21 @@
 package com.example.springwebapp.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+                inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book(String title, String isbn, Set<Author> authors) {
@@ -15,6 +25,14 @@ public class Book {
     }
 
     public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -39,5 +57,28 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
